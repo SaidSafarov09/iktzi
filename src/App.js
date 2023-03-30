54fe8d19a9a872abf81607f9889c0f1e39f1319f
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2023-03-30 03:05:51 +0300

    [IKTZI] iktzi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/main.scss';
+import arrow from './img/arrow.svg';
+import arrowtop from './img/arrow-top.svg';
+import points from './img/points.svg';
+import brand from './img/brand.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='welcome'>
+          <span>Добрый<br></br>вечер</span>
+          <div className='welcome_arrow'>
+            <a href='#content'><img src={arrowtop}/></a>
+          </div>
+          <div className='welcome_content'>
+            <div className='welcome_text'>
+              <p>
+                Это ваше официальное&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br></br>
+                приглашение на&nbsp;<br></br>
+                презентацию новой<br></br> 
+                разработки компании<br></br> 
+                IKTeasy.<br></br> 
+                <br></br>
+              </p>
+            </div> 
+          </div>   
+      </div>
+      <div className='content' id='content'>
+        <div className='content_text'>
+            <p>
+            Мы готовы представить<br></br> 
+            вашему вниманию<br></br> 
+            совершенно новую<br></br> 
+            модель искусственного<br></br> 
+            интеллекта, во всех<br></br> 
+            аспектах превосходящую,<br></br> 
+            то что вы видели<br></br> 
+            раньше.<br></br> 
+            <br></br>
+            Вам откроются глаза, на<br></br> 
+            совершенно новые<br></br> 
+            вариации понятия<br></br> 
+            «творчество» и именно вы<br></br> 
+            станете первыми,<br></br> 
+            заставшими шаг<br></br> 
+            человечества в новую эру<br></br> 
+            возможностей.<br></br> 
+            <br></br>
+            Будем ждать вас<br></br> 
+            21 марта в 18:00<br></br> 
+            в КДК им. Ленина<br></br> 
+            <br></br>
+            Сделаем шаг к будущему<br></br> 
+            вместе!
+            </p>
+            <div className='images'>
+              <img src={arrow}/>
+              <img src={points}/>
+            </div>
+            <div className='brand'>
+                <img src={brand}/>
+                <a href='https://vk.com/ss_iktzi'>Студенческий совет<br></br>ИКТЗИ</a>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
